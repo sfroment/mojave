779d60637f55eecf064a559c64b9aee07aa20f81
kanet <zombiedelah@gmail.com>
2025-04-01 23:02:05 +0900

    Backend APIs

--- crates/abci/src/api.rs
+++ crates/abci/src/api.rs
@@ -1,1 +1,21 @@
-pub trait AbciApi {}
+use crate::types::*;
+
+pub trait AbciApi {
+    type Error: std::error::Error + Send + 'static;
+
+    fn init_chain(&self, request: RequestInitChain) -> Result<ResponseInitChain, Self::Error>;
+
+    fn check_tx(&self, request: RequestCheckTx) -> Result<ResponseCheckTx, Self::Error>;
+
+    fn commit(&self, request: RequestCommit) -> Result<ResponseCommit, Self::Error>;
+
+    fn prepare_proposal(
+        &self,
+        request: RequestPrepareProposal,
+    ) -> Result<ResponsePrepareProposal, Self::Error>;
+
+    fn process_proposal(
+        &self,
+        request: RequestProcessProposal,
+    ) -> Result<ResponseProcessProposal, Self::Error>;
+}
