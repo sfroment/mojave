291993a7fdc14b5c6f6605cfd556215cbc57f8f6
kanet <zombiedelah@gmail.com>
2025-04-08 19:44:13 +0900

    Implement server handle

--- crates/rpc/src/server/mod.rs
+++ crates/rpc/src/server/mod.rs
@@ -8,7 +8,12 @@ use crate::{
 };
 use http::HttpServer;
 use jsonrpsee::server::ServerHandle;
-use std::marker::PhantomData;
+use std::{
+    future::Future,
+    marker::PhantomData,
+    pin::Pin,
+    task::{Context, Poll},
+};
 use websocket::WebsocketServer;
 
 pub struct RpcServer<T>
@@ -35,4 +40,38 @@ where
 pub struct RpcServerHandle {
     http_server_handle: ServerHandle,
     websocket_server_handle: ServerHandle,
+}
+
+impl Future for RpcServerHandle {
+    type Output = RpcServerStatus;
+
+    fn poll(self: Pin<&mut Self>, _cx: &mut Context<'_>) -> Poll<Self::Output> {
+        let this = self.get_mut();
+
+        if this.http_server_handle.is_stopped() {
+            let _ = this.websocket_server_handle.stop();
+            return Poll::Ready(RpcServerStatus::HttpStopped);
+        }
+
+        if this.websocket_server_handle.is_stopped() {
+            let _ = this.http_server_handle.stop();
+            return Poll::Ready(RpcServerStatus::WebsocketStopped);
+        }
+
+        Poll::Pending
+    }
+}
+
+pub enum RpcServerStatus {
+    HttpStopped,
+    WebsocketStopped,
+}
+
+impl std::fmt::Debug for RpcServerStatus {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Self::HttpStopped => write!(f, "RPC(HTTP) server stopped"),
+            Self::WebsocketStopped => write!(f, "RPC(Websocket) server stopped"),
+        }
+    }
 }
