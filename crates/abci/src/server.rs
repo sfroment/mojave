85ac39114a11fcb661771b357b07f5d1571dce80
kanet <zombiedelah@gmail.com>
2025-04-30 01:23:27 +0900

    Add config

--- crates/abci/src/server.rs
+++ crates/abci/src/server.rs
@@ -3,11 +3,13 @@ use futures::FutureExt;
 use std::{
     future::Future,
     marker::PhantomData,
+    path::Path,
     pin::Pin,
     task::{Context, Poll},
     thread::{self, JoinHandle as ThreadJoinHandle},
 };
 use tendermint_abci::ServerBuilder;
+use tendermint_config::TendermintConfig;
 use tokio::{process::Command, task::JoinHandle};
 
 pub struct AbciServer<T>
@@ -21,6 +23,27 @@ impl<T> AbciServer<T>
 where
     T: AbciApi,
 {
+    fn init_config(
+        home_directory: impl AsRef<Path>,
+    ) -> Result<(String, TendermintConfig), AbciServerError> {
+        let home_directory_str = home_directory
+            .as_ref()
+            .to_str()
+            .ok_or(AbciServerError::EmptyHomeDirctory)?;
+
+        let mut cometbft_node = Command::new("cometbft");
+        cometbft_node.args(["init", "--home", home_directory_str]);
+
+        let config_path = home_directory.as_ref().join("config").join("config.toml");
+        let config =
+            TendermintConfig::load_toml_file(&config_path).map_err(AbciServerError::Config)?;
+        if config.consensus.timeout_commit.as_secs() == 0 {
+            return Err(AbciServerError::TimeoutCommitIsZero);
+        }
+
+        Ok((home_directory_str.to_owned(), config))
+    }
+
     pub fn start_cometbft_node(
         home_directory: impl AsRef<str>,
         proxy_app_address: impl AsRef<str>,
@@ -47,11 +70,13 @@ where
     }
 
     pub fn init(
-        home_directory: impl AsRef<str>,
+        home_directory: impl AsRef<Path>,
         address: impl AsRef<str>,
         buffer_size: usize,
         backend: T,
     ) -> Result<AbciServerHandle, AbciServerError> {
+        let (home_directory, _config) = Self::init_config(home_directory)?;
+
         let server = ServerBuilder::new(buffer_size)
             .bind(address.as_ref(), backend)
             .map_err(AbciServerError::Build)?;
@@ -108,6 +133,9 @@ impl Future for AbciServerHandle {
 }
 
 pub enum AbciServerError {
+    EmptyHomeDirctory,
+    Config(tendermint_config::Error),
+    TimeoutCommitIsZero,
     Build(tendermint_abci::Error),
     Server(Option<tendermint_abci::Error>),
     CometBft(String),
@@ -119,6 +147,9 @@ pub enum AbciServerError {
 impl std::fmt::Debug for AbciServerError {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match self {
+            Self::EmptyHomeDirctory => write!(f, "Provide CometBFT home directory"),
+            Self::Config(error) => write!(f, "CometBFT config error: {}", error),
+            Self::TimeoutCommitIsZero => write!(f, "Set timeout_commit to value other than zero"),
             Self::Build(error) => write!(f, "Failed to build ABCI server: {}", error),
             Self::Server(error) => match error {
                 Some(error) => write!(f, "ABCI server stopped with an error: {}", error),
