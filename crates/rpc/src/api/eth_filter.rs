c827d0b88a36e7895d65902deb82f52a716bfd6c
kanet <zombiedelah@gmail.com>
2025-04-29 23:47:59 +0900

    Change FilterId to String

--- crates/rpc/src/api/eth_filter.rs
+++ crates/rpc/src/api/eth_filter.rs
@@ -1,4 +1,4 @@
-use mandu_types::rpc::{Filter, FilterChanges, FilterId, Log};
+use mandu_types::rpc::{Filter, FilterChanges, Log};
 
 #[trait_variant::make(EthFilterApi: Send)]
 pub trait LocalEthFilterApi: Clone + Send + Sync + 'static {
