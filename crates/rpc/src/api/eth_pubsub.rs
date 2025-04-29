d5fd19826174a95d2d6d3bfecb41bd429df27344
kanet <zombiedelah@gmail.com>
2025-04-30 01:34:47 +0900

    WIP: log subscription

--- crates/rpc/src/api/eth_pubsub.rs
+++ crates/rpc/src/api/eth_pubsub.rs
@@ -1,9 +1,12 @@
 use futures::stream::Stream;
-use mandu_types::rpc::{Filter, Header, Log, TransactionHash};
+use mandu_types::{
+    network::AnyHeader,
+    rpc::{Filter, Header, Log, TransactionHash},
+};
 
 #[trait_variant::make(EthPubSubApi: Send)]
 pub trait LocalEthPubSubApi: Clone + Send + Sync + 'static {
-    fn subscribe_new_heads(&self) -> impl Stream<Item = Header> + Unpin;
+    fn subscribe_new_heads(&self) -> impl Stream<Item = Header<AnyHeader>> + Unpin;
 
     fn subscribe_logs(&self, filter: Option<Box<Filter>>) -> impl Stream<Item = Log> + Unpin;
 
