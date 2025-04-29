bb178b8309a8fc3f74958917c20385f0904e7d32
kanet <zombiedelah@gmail.com>
2025-04-30 02:41:52 +0900

    Change initialization parameters

--- crates/node/src/backend/mod.rs
+++ crates/node/src/backend/mod.rs
@@ -24,19 +24,15 @@ impl Clone for Backend {
 }
 
 impl Backend {
-    pub async fn init() -> (Self, anvil::NodeHandle) {
-        let node_config = anvil::NodeConfig::empty_state();
-        let (eth_api, handle) = anvil::try_spawn(node_config).await.unwrap();
-
-        let backend = Self {
+    pub fn init(evm_client: anvil::eth::EthApi, abci_client: AbciClient) -> Self {
+        Self {
             inner: Arc::new(BackendInner {
-                evm_client: eth_api,
-                abci_client: AbciClient::new("http://127.0.0.1:26657").unwrap(),
+                evm_client,
+                abci_client,
                 abci_service: AbciService::init(),
                 pubsub_service: PubSubService::default(),
             }),
-        };
-        (backend, handle)
+        }
     }
 
     pub fn evm_client(&self) -> &anvil::eth::EthApi {
