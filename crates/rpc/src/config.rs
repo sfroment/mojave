4d4f98fbf32b15bf8ac25cb2e0d7656bb57d6532
kanet <zombiedelah@gmail.com>
2025-05-01 15:45:18 +0900

    Config v0.38

--- crates/rpc/src/config.rs
+++ crates/rpc/src/config.rs
@@ -3,3 +3,12 @@ pub struct RpcConfig {
     pub rpc_address: String,
     pub websocket_address: String,
 }
+
+impl Default for RpcConfig {
+    fn default() -> Self {
+        Self {
+            rpc_address: "".to_owned(),
+            websocket_address: "".to_owned(),
+        }
+    }
+}
